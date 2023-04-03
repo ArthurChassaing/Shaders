ed936b3ccd0c78f29e200edab8f14bd231dc22db
Lucas Vocanson <lvocanson@gaming.tech>
2023-04-03 10:43:52 +0200

    Exercices

--- Assets/Scripts/SimpleMeshGenerator.cs
+++ Assets/Scripts/SimpleMeshGenerator.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SimpleMeshGenerator : MonoBehaviour
@@ -8,16 +6,57 @@ public class SimpleMeshGenerator : MonoBehaviour
 
     void Start()
     {
-        //MakeTriangle();
-        
+        MakeTriangle();
+        MakeQuad(); 
+        MakeDoubleQuad();
     }
 
     void MakeTriangle()
     {
-		// TO DO: Vertices array of type Vector3
-		// TO DO: Indices array of type int
-        
-		// TO DO: appeller la fonction BuildMesh avec les bons paramètres
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3 (0.0f, 0.0f, 0.0f),
+            new Vector3 (0.0f, 1.0f, 0.0f),
+            new Vector3 (1.0f, 0.0f, 0.0f)
+        };
+
+        int[] indices = new int[] { 0, 1, 2 };
+
+        BuildMesh("Triangle", vertices, indices);
+    }
+
+    void MakeQuad()
+    {
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3 (2.0f, 0.0f, 0.0f),
+            new Vector3 (2.0f, 1.0f, 0.0f),
+            new Vector3 (3.0f, 1.0f, 0.0f),
+            new Vector3 (3.0f, 0.0f, 0.0f)
+        };
+
+        int[] indices = new int[] { 0, 1, 2, 0, 2, 3 };
+
+        BuildMesh("Quad", vertices, indices);
+    }
+
+    void MakeDoubleQuad()
+    {
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3 (0.0f, 2.0f, 0.0f),
+            new Vector3 (0.0f, 3.0f, 0.0f),
+            new Vector3 (1.0f, 3.0f, 0.0f),
+            new Vector3 (1.0f, 2.0f, 0.0f),
+            new Vector3 (2.0f, 2.0f, 0.0f),
+            new Vector3 (2.0f, 3.0f, 0.0f),
+            new Vector3 (3.0f, 3.0f, 0.0f),
+            new Vector3 (3.0f, 2.0f, 0.0f)
+        };
+
+        int[] indices = new int[] { 0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7 };
+
+        BuildMesh("DoubleQuad", vertices, indices);
     }
 
     protected void BuildMesh(string gameObjectName, Vector3[] vertices, int[] indices, Vector2[] uvs = null)
@@ -29,11 +68,11 @@ public class SimpleMeshGenerator : MonoBehaviour
 
         // Create a GameObject
         GameObject primitive = new GameObject(gameObjectName);
-        
+
         // Add the components...
         MeshRenderer meshRenderer = primitive.AddComponent<MeshRenderer>();
         MeshFilter meshFilter = primitive.AddComponent<MeshFilter>();
-      
+
         // ... and set the mesh buffers. 
         meshFilter.mesh.vertices = vertices;
         meshFilter.mesh.triangles = indices;
--- Assets/Scripts/TorusGenerator.cs
+++ Assets/Scripts/TorusGenerator.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class TorusGenerator : SimpleMeshGenerator  
+public class TorusGenerator : SimpleMeshGenerator
 {
     [Range(3, 30)]
     public int TorusSides = 3;
@@ -29,6 +27,26 @@ public class TorusGenerator : SimpleMeshGenerator
 
     void MakeTorus()
     {
-        //BuildMesh("Torus", vertices.ToArray(), indices.ToArray());
+        Vector3[] vertices = new Vector3[TorusSides * 2];
+        int[] indices = new int[TorusSides * 6];
+        float angle, x, y;
+
+        for (int i = 0; i < vertices.Length; i += 2)
+        {
+            angle = i * Mathf.PI / TorusSides;
+            x = Mathf.Cos(angle) * TorusRadius;
+            y = Mathf.Sin(angle) * TorusRadius;
+            vertices[i] = new Vector3(x, y, TorusHeight);
+            vertices[i + 1] = new Vector3(x, y, 0);
+
+            indices[i * 3] = i;
+            indices[i * 3 + 1] = i + 1;
+            indices[i * 3 + 2] = (i + 2) % (TorusSides * 2);
+            indices[i * 3 + 3] = i + 1;
+            indices[i * 3 + 4] = (i + 3) % (TorusSides * 2);
+            indices[i * 3 + 5] = (i + 2) % (TorusSides * 2);
+        }
+
+        BuildMesh("Torus", vertices, indices);
     }
 }
