e0e6aa1ff94b5c06d493a1cea49ee0835955de08
Juuunnne <92313152+Juuunnne@users.noreply.github.com>
2023-04-07 14:21:58 +0200

    Main
    Saved Scene
    Spawner : spawn delay between every enemies

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject enemyPrefab; // Assignez le prefab d'ennemi dans l'éditeur Unity
-    [SerializeField] private Transform spawnPoint; // Assignez un Transform pour déterminer le point d'apparition
-    [SerializeField] private float spawnInterval = 40f; // Intervalle en secondes entre les vagues d'apparitions
-    [SerializeField] private int enemiesPerWave = 15; // Nombre d'ennemis à faire apparaître par vague
-    [SerializeField] private float delayBetweenEnemies = 1.5f; // Délai en secondes entre chaque ennemi
+    [SerializeField] private GameObject enemyPrefab; 
+    [SerializeField] private Transform spawnPoint; 
+    [SerializeField] private float spawnInterval = 40f; 
+    [SerializeField] private int enemiesPerWave = 15; 
+    [SerializeField] private float delayBetweenEnemies = 1.5f; 
 
     private void Start()
     {
@@ -19,14 +19,11 @@ public class EnemySpawner : MonoBehaviour
     {
         while (true)
         {
-            // Faire apparaître 15 ennemis dans une boucle
             for (int i = 0; i < enemiesPerWave; i++)
             {
                 Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
                 yield return new WaitForSeconds(delayBetweenEnemies);
             }
-
-            // Attendre 20 secondes avant de faire apparaître la prochaine vague d'ennemis
             yield return new WaitForSeconds(spawnInterval);
         }
     }
