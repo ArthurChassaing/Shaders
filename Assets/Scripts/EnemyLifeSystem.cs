5daeff2637660d7536eb985206f8cd24b0bd7130
ArthurChassaing <94675172+ArthurChassaing@users.noreply.github.com>
2023-04-11 19:26:58 +0200

    try to use shader in game (not work)

--- Assets/Scripts/EnemyLifeSystem.cs
+++ Assets/Scripts/EnemyLifeSystem.cs
@@ -6,12 +6,30 @@ using UnityEngine;
 public class EnemyDestroy : MonoBehaviour
 {
     private int _enemyHP = 100;
+    private Renderer shaders;
+    private WaitForSeconds wait = new WaitForSeconds(1);
 
+    public void Start()
+    {
+        shaders = GetComponentInChildren<Renderer>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
         _enemyHP -= 50;
-        
-        if(_enemyHP <= 0)
+    }
+
+    IEnumerator Test()
+    {
+        Debug.Log("TEST");
+        shaders.material.SetFloat("isAnimated", 1);
+        yield return new WaitForSeconds(10);
+    }
+    private void Update()
+    {
+        if (_enemyHP <= 0)
+        {
+            StartCoroutine(Test());
             Destroy(gameObject);
+        }
     }
 }
