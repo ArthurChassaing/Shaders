591eae18b744511ff0dbdfbd227c03fb44cddc9d
Karma <agrandpaul@gaming.tech>
2023-04-11 19:27:18 +0200

    feat : adjustement for spinning orb

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Rotate : MonoBehaviour
 {
     [SerializeField] float _speed = 10f;
+    [SerializeField] Vector3 _axis = Vector3.up;
+    private Rigidbody _rigidbody;
+    
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
     
     void Update()
     {
-        transform.Rotate(Vector3.up, _speed * Time.deltaTime);
+        if (_rigidbody == null)
+            transform.Rotate(_axis * _speed * Time.deltaTime);
+        else
+            _rigidbody.MoveRotation(_rigidbody.rotation * Quaternion.Euler(_axis * _speed * Time.deltaTime));
     }
 }
